1055e4b3130bfe6071e46737159c35f3402c9640
diegod21 <108184501+diegod21@users.noreply.github.com>
2023-05-22 13:59:53 -0300

    testing speedometer

--- speedometer.js
+++ speedometer.js
@@ -1,16 +1,31 @@
 const stopBtn = document.querySelector("#stop");
 const startBtn = document.querySelector("#start");
-const speedElement = document.querySelector("#speed");
+const speedElement = document.querySelector("#speedy");
 
-startBtn.addEventListener("click" , ()=>{
+let watchID = null
+startBtn.addEventListener("click", () => {
+    if(watchID)
+      return
+    function handleSuccess(position){
+        speedElement.innerText = position.coords.speed ? (position.coords.speed *3.6).toFixed(1) :0
+    }
+    function handleError(error){
+        console.log(error.msg)
+    }
+    const options = {enableHighAccuracy:true}
+    watchID = navigator.geolocation.watchPosition(handleSuccess,handleError,options)
 
     startBtn.classList.add("d-none");
     stopBtn.classList.remove("d-none");
 
 })
 
-stopBtn.addEventListener("click" , ()=>{
+stopBtn.addEventListener("click", ()=>{
+    if(!watchID)
+        return
 
+    navigator.geolocation.clearWatch(watchID)
+    watchID = null
     startBtn.classList.remove("d-none");
     stopBtn.classList.add("d-none");
 
