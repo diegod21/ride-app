f38371635b8311500943d9d15edebf2f551ffbe8
diegod21 <108184501+diegod21@users.noreply.github.com>
2023-05-26 16:12:08 -0300

    .

--- index.js
+++ index.js
@@ -18,10 +18,13 @@ allRide.forEach(async ([rideID, value])=>{
 
     const Speed = document.createElement("div")
     maxSpeed = getMaxspeed(ride.data)
-    Speed.innerText = maxSpeed
+    Speed.innerText = `Max Speed:${maxSpeed} Km/h`
 
-    
+    const distance = document.createElement("div")
+    distance.innerText = `Distancia:${getDistance(ride.data)} Km`
+
     itemElement.appendChild(divCity)
+    itemElement.appendChild(distance)
     itemElement.appendChild(Speed)
     rideListElement.appendChild(itemElement)
 })
@@ -35,13 +38,51 @@ async function getLocationData(latitude,longitude){
     return await response.json()
 
 }
-function getMaxspeed(speed){
-    console.log(speed)
+function getMaxspeed(position){
+    console.log(position)
     let maxSpeed = 0;
-    speed.forEach(()=>{
-        if (speed.speed != null && speed.Speed > maxSpeed){
-            maxSpeed = speed.speed
+    position.forEach((position)=>{
+        if (position.speed != null && position.speed > maxSpeed){
+            maxSpeed = position.speed
         } 
-        return maxSpeed;
+        
     })
+    return (maxSpeed *3.6).toFixed(1);
+}
+function getDistance(positions) {
+    const earthRadiusKm = 6371
+    let totalDistance = 0
+    for (let i = 0; i < positions.length - 1; i++) {
+        const p1 = {
+            latitude: positions[i].latitude,
+            longitude: positions[i].longitude
+        }
+        const p2 = {
+            latitude: positions[i + 1].latitude,
+            longitude: positions[i + 1].longitude
+        }
+
+        const deltaLatitude = toRad(p2.latitude - p1.latitude)
+        const deltaLongitude = toRad(p2.longitude - p1.longitude)
+
+        const a = Math.sin(deltaLatitude / 2) *
+            Math.sin(deltaLatitude / 2) +
+            Math.sin(deltaLongitude / 2) *
+            Math.sin(deltaLongitude / 2) *
+            Math.cos(toRad(p1.latitude)) *
+            Math.cos(toRad(p2.latitude))
+
+        const c = 2 * Math.atan2(Math.sqrt(a),
+            Math.sqrt(1 - a))
+
+        const distance = earthRadiusKm * c
+
+        totalDistance += distance
+    }
+
+    function toRad(degree) {
+        return degree * Math.PI / 180
+    }
+
+    return totalDistance.toFixed(2)
 }
